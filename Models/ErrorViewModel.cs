b46a8fdd20fb9015315492cb3d4c0c97314bc28d
kou20xiong30 <138792547+kou20xiong30@users.noreply.github.com>
2023-07-19 11:18:37 -0400

    1st commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MVCWebApp_CloneToVSCode.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
